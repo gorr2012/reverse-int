70a569aee65cc651a02b46b05a48d8f2521c4611
gorr2012 <sergey_gornost@mail.ru>
2021-01-12 16:27:40 +0300

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let i = 1;
+    let res = '';
+    n = Math.abs(n).toString();
+    while(i <= n.length){
+        res += n[n.length - i];
+        i++;
+    }
+    return res;
 }
